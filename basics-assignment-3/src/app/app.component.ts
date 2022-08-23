2bec33609e0443cbb5150019f963d2e1fa118054
Emerson Freitas <emersonfreitasn@gmail.com>
2022-08-23 12:56:45 -0500

    practicing directives solution

--- basics-assignment-3/src/app/app.component.ts
+++ basics-assignment-3/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics-assigment-3';
+  showSecret: boolean = false;
+  log: any = [];
+
+  onToggle() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
